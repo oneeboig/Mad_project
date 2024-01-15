a66bce637d5261343041d68a6c663d7c7baa14db
Mr_OIG <oneebishfaq9@gmail.com>
2024-01-15 20:09:29 +0500

    Home,notification,404_page,Rate_us,Verify_Notification,Helplines pages all are add

--- app/src/main/java/com/example/myapplication/page_404.java
+++ app/src/main/java/com/example/myapplication/page_404.java
@@ -3,15 +3,13 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageView;
 
-public class Home_page extends AppCompatActivity {
-    ImageView home_menu_btn , btn_notifications;
-    Button btn_alert , btn_map , btn_image , btn_video , btn_rate_us;
+public class page_404 extends AppCompatActivity {
+    ImageView back_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_page404);
     }
 }
