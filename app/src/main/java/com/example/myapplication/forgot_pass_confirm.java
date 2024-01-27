1ae7397611516a890c3e0467ad1f21f2401dddf9
Iqra <iq@mail.com>
2024-01-27 13:03:03 +0500

    last screen of forgot password added

---
+++ app/src/main/java/com/example/myapplication/forgot_pass_confirm.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgot_pass_confirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass_confirm);
+    }
+}
